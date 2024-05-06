66f402351398034554fc1c4d606858ffd8ef744b
Sami Männistö <sami.mannisto1@gmail.com>
2024-05-06 11:44:53 +0300

    Add base for the project

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import "./globals.css";
+
+import { RootLayoutProps } from "@/types";
+
+const RootLayout = ({ children }: RootLayoutProps) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main>
+      <h1 className="text-slate-600">This is a test!</h1>
+    </main>
+  );
+};
+
+export default Home;
